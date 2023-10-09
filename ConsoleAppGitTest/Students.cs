41c2928ff8be0d41144bb6a055f3368c6d640fe4
Антон Игоревич Паокин <@kp15.lan>
2023-10-09 09:37:47 +0300

    Add class Students commit

---
+++ ConsoleAppGitTest/Students.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGitTest
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Students() { }
+
+        public Students(int id, string name, int age)
+        {
+            Id = id;
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Age = age;
+        }
+    }
+}
